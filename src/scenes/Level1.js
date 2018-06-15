3caf0c978b0ea0a34dd2820178a85d51ed9bfc1d
DmitryScaletta <dmitryscaletta@mail.ru>
2018-06-15 06:03:41 +0300

    Change camera offset

--- src/scenes/Level1.js
+++ src/scenes/Level1.js
@@ -63,6 +63,7 @@ export default class extends Phaser.Scene {
     };
     this.cameras.main.setBounds(0, 0, this.map.widthInPixels, this.map.heightInPixels);
     this.cameras.main.startFollow(this.player);
+    this.cameras.main.setFollowOffset(0, 40);
 
     this.createHud();
     this.updatePlayerHp();
